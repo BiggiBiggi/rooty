9883e008ddfa691310dc6863f7d22a1de98c2c7f
BiggiBiggi <frelandbenjamin99@gmail.com>
2024-07-09 11:28:05 +0200

    Header and Question

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-//src/App.jsx
+import "./App.css"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/";
 import Survey from "./pages/Survey/";
-
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/survey" element={<Survey />} />
+          <Route path="/survey/:questionNumber" element={<Survey />} />
         </Routes>
       </Router>
     </>

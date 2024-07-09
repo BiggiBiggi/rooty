e3f2da182724380f288089e9f1fe48e94be6110f
BiggiBiggi <frelandbenjamin99@gmail.com>
2024-07-09 13:41:13 +0200

    End of the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-import "./App.css"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/";
-import Survey from "./pages/Survey/";
-
+// src/App.jsx
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import Error from "./components/Error404/index";
+import Header from "./components/Header/index";
+import Freelances from "./pages/Freelances";
+import Home from "./pages/Home/index";
+import Survey from "./pages/Survey";
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/survey" element={<Survey />} />
           <Route path="/survey/:questionNumber" element={<Survey />} />
+          <Route path="/profils" element={<Freelances />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </Router>
     </>
   );
 }
+
 export default App;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,27 @@
+//src/components/Header/index.jsx
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import colors from "../../utils/style/colors";
+console.log(colors);
+
+const StyledLink = styled(Link)`
+  padding: 15px;
+  color: #8186a0;
+  text-decoration: none;
+  font-size: 18px;
+  ${(props) =>
+    props.$isFullLink &&
+    `color: white; border-radius: 30px; background-color: ${colors.primary};`}
+`;
+
 function Header() {
   return (
     <nav>
-      <Link to="/">Accueil</Link>
-      <Link to="/survey/42">Questionnaire</Link>
+      <StyledLink to="/">Accueil</StyledLink>
+      <StyledLink to="/survey/1" $isFullLinks>
+        Questionnaire
+      </StyledLink>
+      <StyledLink to="/profils">Profils</StyledLink>
     </nav>
   );
 }
